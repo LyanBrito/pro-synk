f52593d7e06879befd4aed388d1f1ef91651ecbb
MarloIkhodi <deivid.serafim03@gmail.com>
2024-11-13 13:07:34 -0300

    header function

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,19 @@
+import { Login, Logo, Lupa } from "../../assets/images";
+
 export default function Header() {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <header>
+            <div>
+                <img src={Logo} alt="Logo for ProSync" />
+                <h1>Pro<span>Sync</span></h1>
+            </div>
+            <div><a href="#">Find freelancers</a><a href="#">Work with us</a></div>
+            <div>
+                <img src={Lupa} alt="Search icon" />
+                <p>Search for work...</p>
+            </div>
+            <a>Login <img src={Login} alt="Login anchor" /></a>
+        </header>
+
     )
 }
