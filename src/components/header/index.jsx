7f6cb0dbd6974438b32c2c1d528fd371ff696fe6
Lyan <lyanbrito613@gmail.com>
2024-11-19 21:32:50 -0300

    chore: changed header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,20 +1,24 @@
 import styles from "./header.module.css";
-import { Login, Logo, Lupa } from "../../assets/images";
+import { Login, Logo, Lupa } from "../../assets/svg";
 
 export default function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <div>
-                <img src={Logo} alt="Logo for ProSync" />
-                <h1>Pro<span>Sync</span></h1>
-            </div>
-            <div><a href="#">Find freelancers</a><a href="#">Work with us</a></div>
-            <div>
-                <img src={Lupa} alt="Search icon" />
-                <input type="text" placeholder=" Search for work..."></input>
-            </div>
-            <a>Login <img src={Login} alt="Login anchor" /></a>
-        </header>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerLogo}>
+        <img src={Logo} alt="Logo for ProSync" />
+        <h1>
+          Pro<span>Sync</span>
+        </h1>
+      </div>
+      <nav>
+        <a href="#">Find freelancers</a>
+        <a href="#">Work with us</a>
+      </nav>
 
-    )
+      <div className={styles.loginContainer}>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+      </div>
+    </header>
+  );
 }
