421b2ba72047ba3c02090b0862ae9a9e43ab44fe
Lyan <lyanbrito613@gmail.com>
2024-11-21 22:54:05 -0300

    wip

---
+++ src/components/carousel/index.jsx
@@ -0,0 +1,16 @@
+import styles from "./carousel.module.css"
+
+export default function Carousel(){
+    return (
+        <section className={styles.carouselContainer}>
+            <h4>WEBSITE</h4>
+            <h4>DESIGN</h4>
+            <h4>MARKETING</h4>
+            <h4>EDITING</h4>
+            <h4>SCRIPTS</h4>
+            <h4>WEBSITE</h4>
+            <h4>DESIGN</h4>
+        </section>
+    )
+
+}
